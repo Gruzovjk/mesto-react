956ba4922379a0423c3799d7094b2a3a53ed14c4
Land Lover <filimonovebay@gmail.com>
2023-02-13 01:30:23 +0400

    feat: настроил открытие/закрытие попапов. Вынес попапы с формой и попап с картинкой в отдельные компоненты. Подключил апи, отрисовал данные с сервера. Вынес компонент Card, прокинул в него хендлер открытия попапа с картинкой через компонент main.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
-export default function Footer() {
+function Footer() {
   return (
     <footer className="footer">
       <p className="footer__author">&copy; 2022. Егор Филимонов</p>
     </footer>
   );
 }
+
+export default Footer;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,32 @@
-export default function Main() {
+import React from "react";
+import {api} from "../utils/Api";
+import Card from "./Card";
+
+function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick}) {
+  const [userName, setUserName] = React.useState("");
+  const [userDescription, setUserDescription] = React.useState("");
+  const [userAvatar, setUserAvatar] = React.useState("");
+  const [cards, setCards] = React.useState([]);
+
+  React.useEffect(() => {
+    Promise.all([api.getUserInfo(), api.getInitialCards()])
+      .then(([userData, cardList]) => {
+        setUserName(userData.name);
+        setUserDescription(userData.about);
+        setUserAvatar(userData.avatar);
+        setCards(cardList);
+      })
+      .catch((err) =>
+        console.log(`При получении данных произошла ошибка: ${err}`)
+      );
+  }, []);
+
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__avatar">
           <img
-            src="#"
+            src={userAvatar}
             title="Ваш аватар"
             alt="Аватар"
             className="profile__avatar-img"
@@ -13,49 +35,36 @@ export default function Main() {
             className="profile__avatar-edit-button"
             type="button"
             title="Редактировать фото профиля"
+            onClick={onEditAvatar}
           ></button>
         </div>
         <div className="profile__info">
-          <h1 className="profile__name">Ваше имя</h1>
+          <h1 className="profile__name">{userName}</h1>
           <button
             className="profile__edit-button"
             type="button"
             title="Редактировать профиль"
+            onClick={onEditProfile}
           ></button>
-          <p className="profile__about">Расскажите о себе</p>
+          <p className="profile__about">{userDescription}</p>
         </div>
         <button
           className="profile__add-button"
           type="button"
           title="Добавить новое место"
+          onClick={onAddPlace}
         ></button>
       </section>
 
       <section className="elements">
         <ul className="elements__list">
-          <template className="card__template">
-            <li className="card">
-              <img src="#" alt="Ваше место" className="card__img" />
-              <div className="card__caption">
-                <h2 className="card__name"></h2>
-                <div className="card__like">
-                  <button
-                    className="card__like-button"
-                    type="button"
-                    title="Мне нравится"
-                  ></button>
-                  <span className="card__like-counter"></span>
-                </div>
-              </div>
-              <button
-                className="card__remove-button"
-                type="button"
-                title="Удалить место"
-              ></button>
-            </li>
-          </template>
+          {cards.map((card) => (
+            <Card card={card} key={card._id} onCardClick={onCardClick} />
+          ))}
         </ul>
       </section>
     </main>
   );
 }
+
+export default Main;
